781c7f54c8a50e9ce88cef86cc61bf83d48926dc
yurigabrich <yuribgabrich@gmail.com>
2024-01-18 08:34:13 -0500

    Practice task of the second activity.

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -28,6 +28,30 @@ function show(){
 
 show();
     
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
+
+// Practice task
+{
+    let x = 10;
+    const y = 20;
+    var z = 30;
+
+    // step 1
+    x = 40;
+    // y = 50; // Throws TypeError
+    z = 60;
+
+    console.log(x, y, z) // Works fine
+}
+
+// console.log(x, y, z) // Throws ReferenceError only for 'x' and 'y'
+console.log(z) // Works fine
+
+// step 2
+x = 70; // new declaration of the variable, now it is a global one
+y = 80; // new declaration of the variable, now it is a global one
+z = 90; // reassignment of the block variable
+
+console.log(x, y, z) // Throws ReferenceError only for 'x' and 'y' if l.53 and l.54 are commented
