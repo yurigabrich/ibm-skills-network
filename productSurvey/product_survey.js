ddeedeeb08306f453ed6b3bb94bffe0acfc8c80c
Yuri Gabrich <yuribgabrich@gmail.com>
2024-01-23 10:01:10 -0500

    Another practice test of the Sixth activity of the Week 1.

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,6 +8,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experienceText').value;
     
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -15,7 +16,9 @@ function submitFeedback() {
     document.getElementById('userJob').innerHTML = job;
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userProductChoice2').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
     
     document.getElementById('userInfo').style.display = 'block';
     alert('Thank you for your valuable feedback')
